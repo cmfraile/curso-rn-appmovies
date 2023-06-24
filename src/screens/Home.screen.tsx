4a9ce83084a7a9e641a8905028dc7d5bdef86383
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-24 11:12:06 +0200

    continuamos con la clase tras terminar lo que queriamos de rtkquery

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 const Home = () => {
 
-    const data = moviesApi.useNowPlayingQuery('lel');
+    const data = moviesApi.useNowPlayingQuery();
 
     useEffect(() => console.log(JSON.stringify(data).substring(0,1000)),[data])
 
--- src/store/moviesSlice.ts
+++ src/store/moviesSlice.ts
@@ -1,7 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Result as movie } from "../interfaces/NowPlaying";
 
 const token:string = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGUzMWY5MjQ4ZmE5M2FkZmM2MWY0ZTJhOThjYmY1ZSIsInN1YiI6IjY0OTU5ZTY1ODgwNTUxMDBhZTFkYjc5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jqJBj3YqeXVPFwcJPzIk78zqHbUQx7iPQBJOD2M7148';
+
+//https://api.themoviedb.org/3/movie/now_playing?language=es-ES
+
+const defaultQueryParams = (url:string) => `${url}?language=es-ES`
 
 const moviesApi = createApi({
     reducerPath:'movies',
@@ -13,8 +18,9 @@ const moviesApi = createApi({
         },
     }),
     endpoints:(builder) => ({
-        nowPlaying:builder.query({
-            query:() => ({url:'/now_playing&language=es-ES'})
+        nowPlaying:builder.query<movie[],void>({
+            query:() => ({url:defaultQueryParams('/now_playing')}),
+            transformResponse: ({results}:any) => results,
         })
     })
 });
